28fb30d471356babaee7fbe6f07bbf6aa990357c
Kanta-sill <59639492+Kanta-sill@users.noreply.github.com>
2020-06-18 12:40:33 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@ if(isset($_POST['pass'])){
 	$pass=$_POST['pass'];
 }
 
-$sql="select * from tblaccount where _user='".$username."' and _pass='".$pass."'";
+$sql="select * from tblaccount1 where _user='".$username."' and _pass='".$pass."'";
 
 $result = pg_query($dbconn, $sql)
 		or
